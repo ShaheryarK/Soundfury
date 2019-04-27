9e247c2b7d76e1bb7f637b24b4ed44fa1f2ac8b9
ShaheryarK <shaheryar.khan090078601@gmail.com>
2019-04-27 11:22:27 +0500

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sound_Fury.Startup))]
+namespace Sound_Fury
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
